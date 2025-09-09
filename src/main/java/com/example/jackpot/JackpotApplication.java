2c25dc312721ff98f7d988c24ee6084865b3012c
Saša Bolić <sasa.bolic@sixhours.io>
2025-09-09 15:55:01 +0200

    Initial commit

---
+++ src/main/java/com/example/jackpot/JackpotApplication.java
@@ -0,0 +1,13 @@
+package com.example.jackpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jackpot/JackpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jackpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
